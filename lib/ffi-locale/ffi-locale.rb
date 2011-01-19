6cbea6aa68966275783a1114e684ac62aa74edaf
Krzysztof Zych <krzysztof.zych@redefine.pl>
2011-01-19 17:42:02 +0100

    Include all locale settings from locale.h.

--- lib/ffi-locale/ffi-locale.rb
+++ lib/ffi-locale/ffi-locale.rb
@@ -6,9 +6,19 @@ module FFILocale
   attach_function :strcoll, [:string, :string], :int
   attach_function :setlocale, [:int, :string], :string
 
-  LC_CTYPE = 0
-  LC_COLLATE = 3
-  LC_ALL = 6
+  LC_CTYPE,
+  LC_NUMERIC,
+  LC_TIME,
+  LC_COLLATE,
+  LC_MONETARY,
+  LC_MESSAGES,
+  LC_ALL,
+  LC_PAPER,
+  LC_NAME,
+  LC_ADDRESS,
+  LC_TELEPHONE,
+  LC_MEASUREMENT,
+  LC_IDENTIFICATION = (0..12).to_a
 
   def self.getlocaleinfo
     info = self.setlocale LC_ALL, nil
