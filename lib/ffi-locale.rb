135304b5c4a6fc686b9e6aeb9d1b36900dbfa179
Krzysztof Zych <krzysztof.zych@redefine.pl>
2011-01-19 17:41:38 +0100

    Setup locale from env when loaded.

--- lib/ffi-locale.rb
+++ lib/ffi-locale.rb
@@ -1,2 +1,8 @@
 require 'ffi'
 require 'ffi-locale/ffi-locale.rb'
+
+# Setup locale from environment when first loaded.
+# See http://pubs.opengroup.org/onlinepubs/007908799/xbd/envvar.html#tag_002_002
+ENV.select { |key, value| key =~ /^LC_/ }.each do |key, value|
+  FFILocale::setlocale FFILocale.const_get(key), value
+end
